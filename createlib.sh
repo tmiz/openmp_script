6d8724f533ea1a0b2ae47eebee79a4280691ae0c
Tomoo Mizukami <tomoo.mizukami@sonymobile.com>
2014-02-21 01:56:26 +0900

    initial commit

---
+++ createlib.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -O https://www.openmprtl.org/sites/default/files/libomp_20131209_oss.tgz
+tar -xvzf libomp_20131209_oss.tgz
+rm libomp_20131209_oss.tgz
+patch -u libomp_oss/tools/check-tools.pl < check-tool.patch 
+cd libomp_oss/
+make compiler=clang
